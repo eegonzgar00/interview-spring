66db967dbddd6d9bee0455561290bbfe7bd25739
Ed <egonzgar00@gmail.com>
2025-10-21 22:50:44 +0200

    agg ignore idea

--- src/main/java/com/example/starter/domain/port/UserRepositoryPort.java
+++ src/main/java/com/example/starter/domain/port/UserRepositoryPort.java
@@ -1,6 +1,6 @@
 package com.example.starter.repository;
 
-import com.example.starter.model.User;
+import com.example.starter.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
