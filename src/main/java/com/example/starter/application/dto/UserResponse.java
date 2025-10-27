8e6404419e720cfcd913a36dccc34ac7bb220093
Ed <egonzgar00@gmail.com>
2025-10-27 22:05:39 +0100

    userresponse logica

--- src/main/java/com/example/starter/application/dto/UserResponse.java
+++ src/main/java/com/example/starter/application/dto/UserResponse.java
@@ -1,4 +1,15 @@
 package com.example.starter.application.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Representa la respuesta devuelta al cliente.
+ */
+@Data
+@Builder
 public class UserResponse {
+    private Long id;
+    private String username;
+    private String email;
 }
