cf68404c59ca901886db022de564e18b43ddec51
Ed <egonzgar00@gmail.com>
2025-11-09 21:07:19 +0100

    add model tournament

--- src/main/java/com/example/starter/domain/model/Tournament.java
+++ src/main/java/com/example/starter/domain/model/Tournament.java
@@ -1,4 +1,29 @@
 package com.example.starter.domain.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "tournaments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Tournament {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private LocalDate startDate;
+
+    @ManyToMany
+    @JoinTable(
+            name = "tournament_teams",
+            joinColumns = @JoinColumn(name = "tournament_id"),
+            inverseJoinColumns = @JoinColumn(name = "team_id")
+    )
+    private List<Team> teams;
 }
