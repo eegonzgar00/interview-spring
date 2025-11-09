b27b56ab914da4d26514d4021667dfed0d6eb4c9
Ed <egonzgar00@gmail.com>
2025-11-09 21:06:44 +0100

    add model team

--- src/main/java/com/example/starter/domain/model/Team.java
+++ src/main/java/com/example/starter/domain/model/Team.java
@@ -1,4 +1,28 @@
 package com.example.starter.domain.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity
+@Table(name = "teams")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    // Un equipo puede tener varios usuarios (jugadores)
+    @ManyToMany
+    @JoinTable(
+            name = "team_members",
+            joinColumns = @JoinColumn(name = "team_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> members;
 }
