c1bc6b21fc6b03fc2c38bb88ae20296c6d83af79
Ed <egonzgar00@gmail.com>
2025-11-11 21:35:48 +0100

    add new methods to torunament port repository

--- src/main/java/com/example/starter/domain/port/TournamentRepositoryPort.java
+++ src/main/java/com/example/starter/domain/port/TournamentRepositoryPort.java
@@ -1,4 +1,12 @@
 package com.example.starter.domain.port;
 
+import com.example.starter.domain.model.Tournament;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface TournamentRepositoryPort {
+    List<Tournament> findAll();
+    Optional<Tournament> findById(Long id);
+    Tournament save(Tournament tournament);
 }
