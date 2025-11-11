3390de2e650ad93166ade82031d00543d7de2b52
Ed <egonzgar00@gmail.com>
2025-11-11 21:35:39 +0100

    add new methods to team port repository

--- src/main/java/com/example/starter/domain/port/TeamRepositoryPort.java
+++ src/main/java/com/example/starter/domain/port/TeamRepositoryPort.java
@@ -1,4 +1,12 @@
 package com.example.starter.domain.port;
 
+import com.example.starter.domain.model.Team;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface TeamRepositoryPort {
+    List<Team> findAll();
+    Optional<Team> findById(Long id);
+    Team save(Team team);
 }
