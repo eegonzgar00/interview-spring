addbd2ca1900448a6653ee645020eaeb3096efbb
Ed <egonzgar00@gmail.com>
2025-11-12 22:02:59 +0100

    add tournament services use repository

--- src/main/java/com/example/starter/application/service/TournamentService.java
+++ src/main/java/com/example/starter/application/service/TournamentService.java
@@ -1,4 +1,20 @@
 package com.example.starter.application.service;
 
+import com.example.starter.domain.model.Tournament;
+import com.example.starter.domain.port.TournamentRepositoryPort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TournamentService {
+    private final TournamentRepositoryPort repo;
+
+    public TournamentService(TournamentRepositoryPort repo) {
+        this.repo = repo;
+    }
+
+    public List<Tournament> listTournaments() { return repo.findAll(); }
+
+    public Tournament saveTournament(Tournament tournament) { return repo.save(tournament); }
 }
