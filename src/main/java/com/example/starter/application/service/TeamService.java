fcaa72b60a2f0a856bf67921ea0f7c754287797a
Ed <egonzgar00@gmail.com>
2025-11-12 22:02:50 +0100

    add team services use repository

--- src/main/java/com/example/starter/application/service/TeamService.java
+++ src/main/java/com/example/starter/application/service/TeamService.java
@@ -1,4 +1,20 @@
 package com.example.starter.application.service;
 
+import com.example.starter.domain.model.Team;
+import com.example.starter.domain.port.TeamRepositoryPort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TeamService {
+    private final TeamRepositoryPort repo;
+
+    public TeamService(TeamRepositoryPort repo) {
+        this.repo = repo;
+    }
+
+    public List<Team> listTeams() { return repo.findAll(); }
+
+    public Team saveTeam(Team team) { return repo.save(team); }
 }
