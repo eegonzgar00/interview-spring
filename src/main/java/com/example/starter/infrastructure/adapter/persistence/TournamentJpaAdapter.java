06f9842c753954bdca50e34107d74122eee35500
Ed <egonzgar00@gmail.com>
2025-11-19 21:43:27 +0100

    add torunametjpaadapter with implemets

--- src/main/java/com/example/starter/infrastructure/adapter/persistence/TournamentJpaAdapter.java
+++ src/main/java/com/example/starter/infrastructure/adapter/persistence/TournamentJpaAdapter.java
@@ -1,4 +1,19 @@
 package com.example.starter.infrastructure.adapter.persistence;
 
-public class TournamentJpaAdapter {
+import com.example.starter.domain.model.Tournament;
+import com.example.starter.domain.port.TournamentRepositoryPort;
+import com.example.starter.infrastructure.repository.SpringDataTournamentRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class TournamentJpaAdapter implements TournamentRepositoryPort {
+    private final SpringDataTournamentRepository repo;
+    public TournamentJpaAdapter(SpringDataTournamentRepository repo) { this.repo = repo; }
+
+    public List<Tournament> findAll() { return repo.findAll(); }
+    public Optional<Tournament> findById(Long id) { return repo.findById(id); }
+    public Tournament save(Tournament t) { return repo.save(t); }
 }
