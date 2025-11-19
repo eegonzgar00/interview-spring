46132ad9c01286a75bc1e5b5f310a155da989516
Ed <egonzgar00@gmail.com>
2025-11-19 21:43:07 +0100

    add jpaadapter with implemets

--- src/main/java/com/example/starter/infrastructure/adapter/persistence/TeamJpaAdapter.java
+++ src/main/java/com/example/starter/infrastructure/adapter/persistence/TeamJpaAdapter.java
@@ -1,4 +1,21 @@
 package com.example.starter.infrastructure.adapter.persistence;
 
-public class TeamJpaAdapter {
+
+import com.example.starter.domain.model.Team;
+import com.example.starter.domain.port.TeamRepositoryPort;
+import com.example.starter.infrastructure.repository.SpringDataTeamRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class TeamJpaAdapter implements TeamRepositoryPort {
+    private final SpringDataTeamRepository repo;
+    public TeamJpaAdapter(SpringDataTeamRepository repo) { this.repo = repo; }
+
+    public List<Team> findAll() { return repo.findAll(); }
+    public Optional<Team> findById(Long id) { return repo.findById(id); }
+    public Team save(Team team) { return repo.save(team); }
 }
+
