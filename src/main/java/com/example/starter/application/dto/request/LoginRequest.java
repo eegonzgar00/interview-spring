f18cf915257d9490fa279a5b9ff69653d775926c
Ed <egonzgar00@gmail.com>
2025-11-26 21:47:32 +0100

    add login request

--- src/main/java/com/example/starter/application/dto/request/LoginRequest.java
+++ src/main/java/com/example/starter/application/dto/request/LoginRequest.java
@@ -1,4 +1,12 @@
 package com.example.starter.application.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
 }
